6d6c94fefd67192a7135b2a6111a8640cdbee249
Basant Refaat <b.refaat@coreations.com>
2026-09-06 14:58:00 +0300

    add: add transaction middleware that opens a single DB transaction per HTTP request and commits it automatically on success or rolls it back on exception/failure

---
+++ Common/Middleware/TransactionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using exam_system.Persistence.Context;
+
+namespace exam_system.Common.Middleware
+{
+    public class TransactionMiddleware : IMiddleware
+    {
+        private readonly AppDbContext _context;
+        public TransactionMiddleware(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            //for get endpoints
+            if (context.Request.Method.Equals("GET", StringComparison.OrdinalIgnoreCase))
+            {
+                // Skip transaction for GET requests
+                await next(context);
+                return;
+            }
+
+            using var transaction = await _context.Database.BeginTransactionAsync();
+            try
+            {
+                await next(context);
+                await _context.SaveChangesAsync();
+                await transaction.CommitAsync();
+            }
+            catch (Exception)
+            {
+                await transaction.RollbackAsync();
+                throw;
+            }
+        }
+    }
+}
