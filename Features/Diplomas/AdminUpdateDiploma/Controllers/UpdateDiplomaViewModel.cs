08dd91a722b97dbb027d7d5614e3aba4468ef5e3
Basant Refaat <b.refaat@coreations.com>
2026-09-08 17:23:59 +0300

    add UpdateDiplomaCommand , Handler and UpdateDiplomaVM with it validators

---
+++ Features/Diplomas/AdminUpdateDiploma/Controllers/UpdateDiplomaViewModel.cs
@@ -0,0 +1,4 @@
+﻿namespace exam_system.Features.Diplomas.AdminUpdateDiploma.Controllers
+{
+    public record UpdateDiplomaViewModel(Guid Id, string Title, string? Description);
+}
---
+++ Features/Diplomas/AdminUpdateDiploma/Handlers/UpdateDiplomaCommandHandler.cs
@@ -0,0 +1,21 @@
+﻿using exam_system.Features.Diplomas.AdminUpdateDiploma.Commands;
+using exam_system.Features.Shared;
+using MediatR;
+
+namespace exam_system.Features.Diplomas.AdminUpdateDiploma.Handlers
+{
+    public class UpdateDiplomaCommandHandler : IRequestHandler<UpdateDiplomaCommand , RequestResponse<Unit>>
+    {
+        private readonly IMediator _mediator;
+        public UpdateDiplomaCommandHandler( IMediator mediator) 
+        { 
+            _mediator = mediator;
+        
+        }
+
+        public Task<RequestResponse<Unit>> Handle(UpdateDiplomaCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
