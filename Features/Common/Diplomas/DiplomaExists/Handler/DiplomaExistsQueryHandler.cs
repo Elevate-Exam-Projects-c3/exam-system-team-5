7845578cfc42f9c3579ef02209c4fc57d65753bc
Basant Refaat <b.refaat@coreations.com>
2026-09-09 17:16:08 +0300

    add DiplomaExistsQuery in Common Folder shared between Features

---
+++ Features/Common/Diplomas/DiplomaExists/Handler/DiplomaExistsQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using exam_system.Domain.Entities.Diplomas;
+using exam_system.Features.Common.Diplomas.DiplomaExists.Query;
+using exam_system.Features.Shared;
+using exam_system.Persistence.DataAccess;
+using MediatR;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace exam_system.Features.Common.Diplomas.DiplomaExists.Handler
+{
+    public class DiplomaExistsQueryHandler : IRequestHandler<DiplomaExistsQuery , RequestResponse<bool>>
+    {
+        private readonly IGenericRepository<Diploma> _diplomaRepository;
+
+        public DiplomaExistsQueryHandler(IGenericRepository<Diploma> diplomaRepository) 
+        {
+            _diplomaRepository = diplomaRepository;
+        }
+
+        public async Task<RequestResponse<bool>> Handle(DiplomaExistsQuery query, CancellationToken cancellationToken)
+        {
+            var isExists =  await _diplomaRepository.ExistsAsync(d => d.Id == query.Id, cancellationToken);
+            return RequestResponse<bool>.Ok(isExists);
+        }
+    }
+}
