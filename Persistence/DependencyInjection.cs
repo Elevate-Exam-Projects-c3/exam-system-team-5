f0cc136fa8daab6eba0fa0891696d0bc2b1470dc
refaatbasant2030 <refaatbasant2030@gmail.com>
2026-09-12 01:07:16 +0300

    end enrol in diploma feature

--- Persistence/DependencyInjection.cs
+++ Persistence/DependencyInjection.cs
@@ -26,11 +26,11 @@ public static class DependencyInjection
         // version 11.0.0 not have any commercial license, so we can use it for free. But version 12.0.0 and above have a commercial license, so we need to use version 11.0.0 for free usage.
         services.AddMediatR(typeof(Program));
 
-
+        //services.AddScoped(typeof(IPipelineBehavior<,>), typeof(SaveChangesBehavior<,>));
         services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
         services.AddScoped<TransactionMiddleware>();
 
-        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        //services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 
 
