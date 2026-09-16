4bda5b468a50a6e0bc8a5d7e3ba51de1f94532a1
Mohamed Elmosel <elmosel44@gmail.com>
2026-09-16 07:45:05 -0700

    implement role-based access control

---
+++ Common/Constants/AppRole.cs
@@ -0,0 +1,8 @@
+﻿namespace exam_system.Common.Constants
+{
+    public static class AppRole
+    {
+        public const string Admin = "Admin";
+        public const string Student = "Student";
+    }
+}
